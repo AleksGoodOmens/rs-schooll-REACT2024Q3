04698763f18008e13c12204d99425b85fc6cb8fe
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-07-06 13:50:55 +0100

    check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,9 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
 		'plugin:react/jsx-runtime',
+		'plugin:@typescript-eslint/recommended',
 		'plugin:prettier/recommended',
 	],
 	settings: {
