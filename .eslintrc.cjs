aba10080664af5ff286c35cd8fe7ccdbe8547015
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-07-06 10:36:14 +0100

    feat: implement eslint and it's plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,21 +3,27 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-		'plugin:@typescript-eslint/recommended-type-checked',
-		'plugin:@typescript-eslint/stylistic-type-checked',
-		'plugin:react-hooks/recommended',
 		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
 		'plugin:react/jsx-runtime'
-  ],
+	],
+	settings: {
+		react: {
+			"version": "detect"
+		}
+
+	},
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh',  "react-compiler"],
+  plugins: ['react','react-refresh',  "react-compiler"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
 		],
 		"react-compiler/react-compiler": "error"
+		
 
   },
 }
