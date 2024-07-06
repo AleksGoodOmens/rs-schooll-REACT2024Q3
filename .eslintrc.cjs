76b7d9b2c0c1a2ffb6b5df1cb042edad1080cc58
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-07-06 10:57:08 +0100

    refactor: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,27 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
 		'plugin:react/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
-		'plugin:react/jsx-runtime'
+		'plugin:react/jsx-runtime',
+		'prettier',
 	],
 	settings: {
 		react: {
-			"version": "detect"
-		}
-
+			version: 'detect',
+		},
 	},
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react','react-refresh',  "react-compiler"],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react', 'react-refresh', 'react-compiler'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
 		],
-		"react-compiler/react-compiler": "error"
-		
-
-  },
+		'react-compiler/react-compiler': 'error',
+	},
 }
