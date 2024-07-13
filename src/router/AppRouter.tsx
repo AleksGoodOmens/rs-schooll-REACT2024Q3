ec7992ad96d810c76f3dd57e53ba2e5c23a63dde
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-07-14 00:02:18 +0100

    feat: implement app router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,33 +1,35 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from '../pages/main/MainPage';
 import NotFoundPage from '../pages/notFound/NotFoundPage';
-import SearchResults from '../components/searchResults/SearchResults';
+import DetailedCard from '../components/detailedCard/DetailedCard';
+import Categories from '../components/categories/Categories';
+import ErrorFallBack from '../components/error/errorFallBack/ErrorFallBack';
+import Category from '../components/category/Category';
 
 const AppRoutes = createBrowserRouter([
 	{
 		path: '/',
 		element: <MainPage />,
+		errorElement: <ErrorFallBack />,
 		children: [
 			{
-				path: '/:category',
-				element: <SearchResults />,
+				path: 'categories',
+				element: <Categories />,
 				children: [
 					{
-						path: '/:category/:name',
-						element: (
-							<h1>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-								distinctio non eaque in accusamus tenetur velit! Earum, veniam
-								tempore dolorum nesciunt praesentium dolore! Dolorem nulla
-								officia impedit nisi molestiae laudantium!
-							</h1>
-						),
+						path: ':dynamicCategory',
+						element: <Category />,
+						children: [
+							{
+								path: ':id',
+								element: <DetailedCard />,
+							},
+						],
 					},
 				],
 			},
 		],
 	},
-
 	{
 		path: '*',
 		element: <NotFoundPage />,
