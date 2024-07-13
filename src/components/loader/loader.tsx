816ae7fa05f536c7d3dc989301bf0f4a4c0a126b
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-07-14 00:05:50 +0100

    fix: updated loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.css';
 const Loader = () => {
 	return (
-		<div className={`${styles.box} fadeIn`}>
+		<div className={`${styles.box} `}>
 			<h2>Loading...</h2>
 			<div className={styles.img}></div>
 		</div>
