c7776a91cf04c825f91e2f040d0fba9867524869
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-08-02 11:19:45 +0100

    feat: add vite.config

--- src/components/layout/footer/Footer.test.tsx
+++ src/components/layout/footer/Footer.test.tsx
@@ -2,12 +2,10 @@ import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
 
-describe('footer', () => {
+describe.only('footer', () => {
 	it('footer is rendered at the page', () => {
 		render(<Footer />);
 
-		const loadingText = screen.getByText('Developed by AmensGood');
-
-		expect(loadingText).toBeInTheDocument();
+		expect(screen.getByText('Developed by AmensGood')).toBeInTheDocument();
 	});
 });
